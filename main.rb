801c89d0d8433954a4c95b1bf745c1471c854d5d
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-07-18 08:20:59 +0000

    Add Decode string

--- main.rb
+++ main.rb
@@ -19,9 +19,8 @@ def decode_word(string)
  string.split('  ').map { |word| word.split.map { |char| decode_char(char) }.join }.join(' ')
 end
 
-# khan
-# def decode(string)
-#   string.split('   ').map { |word| decode_word(word) }.join(' ')
-# end
+def decode(string)
+  string.split('   ').map { |word| decode_word(word) }.join(' ')
+end
 
 p decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...') # ABOXFULLOFRUBIES
